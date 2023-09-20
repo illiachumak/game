0125fd553ded6ff900daa08e170f34a911d9296b
illiachumak <illia.chumak.2017@gmail.com>
2023-09-20 17:47:35 +0300

    'Hover squares' table changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [selectedMode, setSelectedMode] = useState(null);
   const [grid, setGrid] = useState([]);
   const [gridSize, setGridSize] = useState(0);
+  const [hoveredSquares, setHoveredSquares] = useState([]);
 
   useEffect(() => {
     axios.get('https://60816d9073292b0017cdd833.mockapi.io/modes')
@@ -30,28 +31,75 @@ function App() {
     const newGrid = [...grid];
     newGrid[i][j] = newGrid[i][j] === 0 ? 1 : 0;
     setGrid(newGrid);
-  };
+
+    const existingSquare = hoveredSquares.find(sq => sq.i === i && sq.j === j);
+
+    if (existingSquare) {
+
+        setHoveredSquares(prev => prev.filter(sq => !(sq.i === i && sq.j === j)));
+    } else {
+        setHoveredSquares(prev => [...prev, { i, j }]);
+    }
+
+    setHoveredSquares(prev => {
+        return [...prev].sort((a, b) => {
+            if (a.i === b.i) {
+                return a.j - b.j;
+            }
+            return a.i - b.i;
+        });
+    });
+};
+
+
 
   return (
     <div className="App">
-      <select onChange={e => setSelectedMode(modes.find(mode => mode.id === e.target.value))}>
+      <select onChange={e => {
+        setSelectedMode(modes.find(mode => mode.id === e.target.value))
+        setHoveredSquares([]); 
+      }}>
         <option>Pick a mode</option>
         {modes.map(mode => <option key={mode.id} value={mode.id}>{mode.name}</option>)}
       </select>
       <button onClick={handleStart} className='start-btn'>START</button>
-      <div className="grid" style={{width: (25 * gridSize) + 'px'}}> 
-        {grid.map((row, i) => (
-          <div key={i} className="grid-row">
-            {row.map((cell, j) => (
-              <div
-                key={j}
-                className={`square ${cell === 1 ? 'blue' : ''}`}
-                onMouseOver={() => handleHover(i, j)}
-              ></div>
-            ))}
-          </div>
-        ))}
+      <div className="grid-container" style={{marginTop: '20px', width: '300px', height: '300px', overflow: 'auto'}}>
+        <div className="grid" style={{width: (25 * gridSize) + 'px'}}> 
+          {grid.map((row, i) => (
+            <div key={i} className="grid-row">
+              {row.map((cell, j) => (
+                <div
+                  key={j}
+                  className={`square ${cell === 1 ? 'blue' : ''}`}
+                  onMouseOver={() => handleHover(i, j)}
+                ></div>
+              ))}
+            </div>
+            
+          ))}
+        </div>
       </div>
+      {hoveredSquares && selectedMode &&(
+        <>
+        <h1>Hover squares</h1>
+        <table>
+        <thead>
+          <tr>
+            <th>Row</th>
+            <th>Column</th>
+          </tr>
+        </thead>
+        <tbody>
+          {hoveredSquares.map((square, index) => (
+            <tr key={index}>
+              <td>{square.i}</td>
+              <td>{square.j}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </>
+      )}
     </div>
   );
 }
